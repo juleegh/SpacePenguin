f3e1cc6adb7b4f27410b27dd2a612a3422aeaed5
juleegh <julymontes495@gmail.com>
2023-09-15 12:08:39 +0200

    Added UI

---
+++ SpacePenguin/Assets/FacePlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 eulerAngles = transform.eulerAngles;
+        transform.LookAt(player.transform.position);
+        eulerAngles.y = transform.eulerAngles.y;
+        transform.eulerAngles = eulerAngles;
+    }
+}
---
+++ SpacePenguin/Assets/GameUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private Button exitPauseButton;
+    [SerializeField] private GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseButton.onClick.AddListener(OnPauseButtonClicked);
+        exitPauseButton.onClick.AddListener(OnExitPauseButtonClicked);
+    }
+
+    private void OnPauseButtonClicked()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    private void OnExitPauseButtonClicked()
+    {
+        pauseMenu.SetActive(false);
+    }
+}
